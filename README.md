d59ebf6a574c1aab1846a57d2beb255d4f447c65
Rebecca Alpert <ralpert@redhat.com>
2023-01-26 11:21:34 -0500

    Add a few more things to the script.

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # new-computer-setup
 
+I have not verified that VSCode and Homebrew installation work as intended, but I have verified that the script will continue running if you have the files already. I would like to refine this script the next time I need to set up a new computer. However, the rest of the script should work as intended.
+
 This repo installs the following:
+* [VSCode](https://code.visualstudio.com/docs?dv=osx)
 * [Homebrew](https://brew.sh/)
+* [nvm](https://github.com/nvm-sh/nvm)
+* [Node](https://nodejs.org)
 * [yarn](https://classic.yarnpkg.com/en/)
 * [thefuck](https://github.com/nvbn/thefuck)
 * [git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
 * [GitHub CLI](https://cli.github.com/)
-
-## Useful resources I haven't added to the script
-* [VSCode](https://code.visualstudio.com/docs?dv=osx)
-* [Node](https://nodejs.org/en/download/)
-
+* [Cypress](https://www.cypress.io/)
 ## VSCode Extensions
 * [Prettier](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode)
 * [ESLint](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)
--- setup.sh
+++ setup.sh
@@ -4,10 +4,32 @@ set -euo pipefail
 
 echo "Installing..."
 
+VSCODE="$HOME/Applications/VSCode-darwin-universal.zip"
+HOMEBREW="/opt/homebrew"
+
 if [ "$(uname)" == "Darwin" ]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    if [ ! -e "$VSCODE" ]; then
+        mkdir -p $HOME/Applications
+        curl -o $VSCODE https://code.visualstudio.com/sha/download\?build=stable&os=darwin-universal 
+        open $VSCODE
+    else
+        echo "Already downloaded VSCode"
+    fi 
+
+    if [ ! e "$HOMEBREW" ]; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else   
+        echo "Already downloaded Homebrew"
+    fi
+
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    nvm install node
     npm install --global yarn
     brew install git
     brew install gh
     brew install thefuck
+    npm install cypress
 fi
